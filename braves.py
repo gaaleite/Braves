889fc7d03a11a436ffe5867be58e17cde517a105
gaaleite <115560069+gaaleite@users.noreply.github.com>
2026-06-09 12:28:36 -0300

    Update braves.py

--- braves.py
+++ braves.py
@@ -1,10 +1,6 @@
 import streamlit as st
 import pandas as pd
-import 
-
-plotly.express
-
-as px
+import plotly.express as px
 
 st.set_page_config(layout="wide", page_title="Braves Analytics")
 st.title("🏈 Braves Academy - Gráfico de Jogos")
